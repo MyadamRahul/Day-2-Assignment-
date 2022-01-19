6d004b6a28f6c281cea64a7f4768210fb2ce10b5
MyadamRahul <96530220+MyadamRahul@users.noreply.github.com>
2022-01-19 22:37:30 +0530

    Day 2 assignment Submitted

---
+++ day2.py
@@ -0,0 +1,31 @@
+#integer
+#string
+age=19
+name='rahul'
+print(age)
+print("my age is %s"%(age))
+print("my name is {name},im {age}".format(name='rahul',age='19'))
+print(f"hello my name is{name},i'm{age}")
+print("")
+#float
+a=5.6
+print(a)
+print("a is %s"%(a))
+print("a is {a}".format(a=7.8))
+print(f"a is {a}")
+print("")
+#list
+list=[1,2,3,4,5]
+print(list)
+print("my list is %s"%(list))
+print("my list is {list}".format(list=[4,5,6,7]))
+print(f"my list is {list}")
+#dictionaries
+print("")
+my_dict={1:'rahul',2:'shiva',3:'shekar'}
+print(my_dict)
+print("my_dict is %s"%(my_dict))
+print("my_dict is {my_dict}".format(my_dict={1:'rahul',2:'shiva',3:'shekar'}))
+print(f"my_dict is {my_dict}")
+
+
